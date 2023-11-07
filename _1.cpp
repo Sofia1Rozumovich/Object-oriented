66deb4896e44eaa7ee9ac804fbf59c3487d828bd
Sofia1Rozumovich <rozumovichsofia@gmail.com>
2023-11-07 19:34:44 +0200

    Added new class Hat. Updated output information. v.2.0

--- _1.cpp
+++ _1.cpp
@@ -3,17 +3,37 @@
 
 using namespace std;
 
+class Hat
+{
+private:
+    string color;
+    string size;
+public:
+    Hat(string inColor, string inSize) : color(inColor), size(inSize) {}
+
+    string GetColor() const
+    {
+        return color;
+    }
+
+    string GetSize() const
+    {
+        return size;
+    }
+};
+
 class Human
 {
 private:
     string name;
     string surname;
+    Hat hat;
 public:
-    Human(string inName, string inSurname) : name(inName), surname(inSurname) {}
+    Human(string inName, string inSurname, Hat inHat) : name(inName), surname(inSurname), hat(inHat) {}
 
-    friend ostream& operator<< (ostream &out, const Human& point)
+    friend ostream& operator<< (ostream &out, const Human& person)
     {
-        out << "I'm " << point.name << " " << point.surname << "!";
+        out << "I'm " << person.name << " " << person.surname << "! I have a " << person.hat.GetColor() << " " << person.hat.GetSize() << " hat!";
         return out;
     }
 };
@@ -23,7 +43,9 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     
-    Human someone("Sofia", "Rozumovich");
+    Hat something("Red", "big");
+    Human someone("Sofia", "Rozumovich", something);
     cout << endl << someone << endl;
+    
     return 0;
 }
